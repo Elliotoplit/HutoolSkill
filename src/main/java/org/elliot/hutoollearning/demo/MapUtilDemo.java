34d973590bd4f45df404800dc0313bc4d34016be
Elliotoplit <614871603@qq.com>
2025-03-16 17:32:52 +0800

    Update For Idea debug and Map

--- src/main/java/org/elliot/hutoollearning/demo/MapUtilDemo.java
+++ src/main/java/org/elliot/hutoollearning/demo/MapUtilDemo.java
@@ -16,12 +16,42 @@ import java.util.*;
 public class MapUtilDemo {
     public static void main(String[] args) {
         Product product = new Product("B_Prod", "0", "100", "100", "300");
-        Product product2 = new Product("C_Prod ", " 1", "10", "20", "30");
-        Product product3 = new Product("C_Prod ", " 2", "10", "20", "30");
-        Product product4 = new Product("C_Prod ", " 2", "10", "20", "30");
-        Product product5 = new Product("D_Prod ", " 1", "10", "20", "30");
+        Product product2 = new Product("C_Prod ", "1", "10", "20", "30");
+        Product product3 = new Product("C_Prod ", "2", "10", "20", "30");
+        Product product4 = new Product("C_Prod ", "2", "10", "20", "30");
+        Product product5 = new Product("D_Prod ",  "1", "10", "20", "30");
         List<Product> dataList = List.of(product, product2, product3, product4, product5);
 
+        // DEBUG TEST
+        String str = "HELLO";
+        Map<String, String> testMap = new HashMap<>(6);
+        {
+            testMap.put("11","北京");
+            testMap.put("12","天津");
+            testMap.put("13","河北");
+            testMap.put("14","山西");
+            testMap.put("15","内蒙古");
+            testMap.put("21","辽宁");
+            testMap.put("22","吉林");
+            testMap.put("23","黑龙江");
+            testMap.put("31","上海");
+            testMap.put("32","江苏");
+            testMap.put("33","浙江");
+            testMap.put("34","安徽");
+            testMap.put("35","福建");
+            testMap.put("36","江西");
+            testMap.put("37","山东");
+            testMap.put("41","河南");
+            testMap.put("42","湖北");
+            testMap.put("43","湖南");
+            testMap.put("44","广东");
+            testMap.put("45","广西");
+            testMap.put("51","四川");
+            testMap.put("52","贵州");
+            testMap.put("53","云南");
+            testMap.put("54","西藏");
+
+        }
         Map<String, List<Product>> listMap = new HashMap<>();
         //Java.Map.putIfAbsent - 按照prodKey分组
         for (Product rowData : dataList) {
@@ -30,7 +60,7 @@ public class MapUtilDemo {
             //listMap.putIfAbsent(prodKey, new ArrayList<>());
             //listMap.get(prodKey).add(rowData);
             //方法2:
-            listMap.compute(prodKey,(key,valueList)->{
+            listMap.compute(prodKey, (key, valueList) -> {
                 if (Objects.isNull(valueList)) {
                     valueList = new ArrayList<>();
                 }
@@ -40,25 +70,22 @@ public class MapUtilDemo {
         }
 
 
-
         //Java.Map.compute - 按照prodName分组
         Map<String, Integer> countMap = new HashMap<>();
         Map<String, Integer> countMapForIfAbsent = new HashMap<>();
         for (Product rowData : dataList) {
             String prodKey = prodKey(rowData.getProdName(), rowData.getProdVersion());
             countMap.compute(prodKey, (key, count) -> {
-                System.out.println("prodKey = " + prodKey );
+                System.out.println("prodKey = " + prodKey);
                 System.out.println("key = " + key);
                 return Objects.isNull(count) ? 1 : count + 1;
             });
             countMapForIfAbsent.computeIfAbsent(prodKey, key -> {
-                System.out.println("prodKey = " + prodKey );
+                System.out.println("prodKey = " + prodKey);
                 System.out.println("key = " + key);
                 return 1;
             });
         }
-
-
 
 
         Map<String, Object> prodMap = BeanUtil.beanToMap(product);
@@ -107,6 +134,9 @@ public class MapUtilDemo {
     }
 
     private static String prodKey(String prodName, String prodVersion) {
-        return prodName + "." + prodVersion;
+        String prodKey = prodName + "." + prodVersion;
+        System.out.println("Other Operations1....");
+        System.out.println("Other Operations2....");
+        return prodKey;
     }
 }
